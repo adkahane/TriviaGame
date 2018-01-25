0c1a30bbdf6496fbfad5dec17a880dffa5e735e1
adkahane <adam.kahane.nj@gmail.com>
2018-01-24 22:14:19 -0800

    start button and some functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
     // ==========================================================================
     // The object questions for our quiz game.
     var questions = {
-      q1: ["The sky is blue.", "t"],
+      q1: ["Which actor has been a vampire in Buffy?", "3"],
       q2: ["There are 365 days in a year.", "t"],
       q3: ["There are 42 ounces in a pound.", "f"],
       q4: ["The Declaration of Independence was created in 1745.", "f"],
@@ -10,9 +10,11 @@
     };
 
     // We start the game with a score of 0.
-    var score = 0;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
     // Variable to hold the index of current question.
-    var questionIndex = 0;
+    var qIndex = 0;
     // Array of questions.
     var questionsArray = [questions.q1, questions.q2, questions.q3, questions.q4, questions.q5];
     console.log(questionsArray)
@@ -22,21 +24,23 @@
 
     // Function to render questions.
     function renderQuestion() {
-      // If there are still more questions, render the next one.
+      //If there are still more questions, render the next one.
 
-      if (questionIndex <= (questionsArray.length - 1)) {
-        document.querySelector("#question").innerHTML = questionsArray[questionIndex][0];
+      if (qIndex <= (questionsArray.length - 1)) {
+        $("#question").html = questionsArray[qIndex][0];
       }
       // If there aren't, render the end game screen.
       else {
-        document.querySelector("#question").innerHTML = "Game Over!";
-        document.querySelector("#score").innerHTML = "Final Score: " + score + " out of " + questionsArray.length;
+        $("#question").html = "Game Over!";
+        $("#score").html = "Final Score: " + score + " out of " + questionsArray.length;
       }
     }
 
-    // Function that updates the score...
-    function updateScore() {
-      document.querySelector("#score").innerHTML = "Score: " + score;
+    // Function that shows final stats
+    function finalStats() {
+      $("#question").html = "<h1>All done, here's how you did!<h1><h3>Correct Answers: </h3>" + correct + "<h3>Incorrect Answers: </h3>" + incorrect + "<h3>unanswered: </h3>" + unanswered;
+
+      $("#choices").append = "<h3 class='ml-auto mr-auto'>Correct Answers: " + correct;
     }
 
 
@@ -44,14 +48,14 @@
     // ==============================================================================
 
     // Calling functions to start the game.
-    renderQuestion();
-    updateScore();
+    // renderQuestion();
+    // updateScore();
 
     // When the user presses a key, it will run the following function...
-    document.onkeyup = function(event) {
+    $("#start").on("click", function() {
 
       // If there are no more questions, stop the function.
-      if (questionIndex === questionsArray.length) {
+      if (qIndex === questionsArray.length) {
         return;
       }
 
@@ -62,7 +66,7 @@
       if (userInput === "t" || userInput === "f") {
 
         // If they guess the correct answer, increase and update score, alert them they got it right.
-        if (userInput === questionsArray[questionIndex][1]) {
+        if (userInput === questionsArray[qIndex][1]) {
           alert("Correct!");
           score++;
           updateScore();
@@ -73,9 +77,9 @@
         }
 
         // Increment the questionIndex variable and call the renderQuestion function.
-        questionIndex++;
+        qIndex++;
         renderQuestion();
 
       }
 
-    };
+    });
